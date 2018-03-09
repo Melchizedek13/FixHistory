7192de82b7a919d03cc6084a8d04ed490ba87347
Melchizedek <nikolayssokolov@gmail.com>
2018-03-09 03:28:01 +0300

    Added clause on the same dates:
    curr start date = next start date
    or
    curr end date = next end date

--- Psql_fix_holes_and_intersects_in_history.sql
+++ Psql_fix_holes_and_intersects_in_history.sql
@@ -1,33 +1,44 @@
 
 /*
-    1. Как исправлять историю записей, которые открыты в один день?
-    2. Как быть с засписями, у которых история одинакова, но значения разные?
+    Как быть с засписями, у которых история одинакова, но значения разные?
 */
 
 with a as (
-    select 1 as id, to_date('01.01.2018', 'dd.mm.yyyy') as sd, to_date('03.01.2018', 'dd.mm.yyyy') as ed, 10 as val union all
-    select 1, to_date('04.01.2018', 'dd.mm.yyyy'), to_date('07.01.2018', 'dd.mm.yyyy'), 11 union all
-    select 1, to_date('11.01.2018', 'dd.mm.yyyy'), to_date('17.01.2018', 'dd.mm.yyyy'), 12 union all
-    select 1, to_date('22.01.2018', 'dd.mm.yyyy'), to_date('01.02.2018', 'dd.mm.yyyy'), 13 union all
-    select 1, to_date('02.02.2018', 'dd.mm.yyyy'), to_date('13.02.2018', 'dd.mm.yyyy'), 14 union all
-    select 1, to_date('08.02.2018', 'dd.mm.yyyy'), to_date('26.02.2018', 'dd.mm.yyyy'), 15 union all
-    select 1, to_date('01.03.2018', 'dd.mm.yyyy'), to_date('31.12.9999', 'dd.mm.yyyy'), 16 union all
+    select 1 as id, to_date('01.01.2018', 'dd.mm.yyyy') as sd, to_date('10.01.2018', 'dd.mm.yyyy') as ed, 11 as val union all
+    select 1, to_date('01.01.2018', 'dd.mm.yyyy'), to_date('08.01.2018', 'dd.mm.yyyy'), 10 union all
+    select 1, to_date('01.01.2018', 'dd.mm.yyyy'), to_date('05.01.2018', 'dd.mm.yyyy'), 9 union all
+    select 1, to_date('13.01.2018', 'dd.mm.yyyy'), to_date('25.01.2018', 'dd.mm.yyyy'), 12 union all
+    select 1, to_date('20.01.2018', 'dd.mm.yyyy'), to_date('25.01.2018', 'dd.mm.yyyy'), 14 union all
+    select 1, to_date('15.01.2018', 'dd.mm.yyyy'), to_date('25.01.2018', 'dd.mm.yyyy'), 13 union all
+    select 1, to_date('02.02.2018', 'dd.mm.yyyy'), to_date('13.02.2018', 'dd.mm.yyyy'), 15 union all
+    select 1, to_date('08.02.2018', 'dd.mm.yyyy'), to_date('26.02.2018', 'dd.mm.yyyy'), 16 union all
+    select 1, to_date('01.03.2018', 'dd.mm.yyyy'), to_date('31.12.9999', 'dd.mm.yyyy'), 17 union all
     select 2, to_date('01.01.2018', 'dd.mm.yyyy'), to_date('31.12.9999', 'dd.mm.yyyy'), 20
-), preHandleA as (
+), sameDatesInA as (
+    select id, sd, ed, val,
+           lead(sd) over (partition by id order by sd, ed desc) as nsd,
+           lag(ed)  over (partition by id order by sd, ed asc)  as ped,
+           lead(sd) over (partition by id order by sd, ed desc) - sd as same_sd,
+           lead(ed) over (partition by id order by sd, ed asc)  - ed as same_ed
+      from a
+), postSameDatesInA as (
+    select id, nsd, val,
+           case when same_sd = 0 then ped + 1 else sd end as sd, 
+           case when same_ed = 0 then nsd - 1 else ed end as ed
+      from sameDatesInA
+), findHolesInA as (
     select id, sd, ed, val,
            lead(sd) over (partition by id order by sd) as nsd,
-           lead(sd) over (partition by id order by sd) - ed - 1 as diff
-      from a
+           lead(sd) over (partition by id order by sd) - ed - 1 as diff 
+      from postSameDatesInA
 ), fixHolesInA as (
-    select id,
-           sd,
+    select id, sd, val, nsd,
            case when diff > 0 then ed + diff else ed end as ed,
-           case when nsd  <= case when diff > 0 then ed + diff else ed end then 1 else 0 end fi,
-           val,
-           nsd
-      from preHandleA
+           case when nsd  <= case when diff > 0 then ed + diff else ed end then 1 else 0 end fi
+      from findHolesInA
 ), fixIntInA as (
-    select id, sd, case when fi = 1 then nsd - 1 else ed end as ed, val
+    select id, sd, val, 
+           case when fi = 1 then nsd - 1 else ed end as ed
       from fixHolesInA
 )
   select id, 
