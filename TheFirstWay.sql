fabff7dca7585c56705931b2468c28608ade96b9
Melchizedek <nikolayssokolov@gmail.com>
2018-03-13 10:56:02 +0300

    Added a link about using the features of functions greatest() and least() in PostgreSQL.

--- TheFirstWay.sql
+++ TheFirstWay.sql
@@ -1,4 +1,9 @@
 
+/*
+  Надо не забывать, что функции GREATEST и LEAST в Postgres, в отличии от реализации в Oracle и DB2 LUW, игнорируют NULL.
+  https://habrahabr.ru/post/340460/#comment_10488792
+*/
+
 with t1(id, sd, ed, x) as (
    select 1, to_timestamp('2018-01-01 00:00:00', 'yyyy-mm-dd hh24:mi:ss'), to_timestamp('2018-01-10 00:00:00', 'yyyy-mm-dd hh24:mi:ss'), 1 union all
    select 2, to_timestamp('2018-02-01 00:00:01', 'yyyy-mm-dd hh24:mi:ss'), to_timestamp('2018-03-01 16:13:13', 'yyyy-mm-dd hh24:mi:ss'), 3
